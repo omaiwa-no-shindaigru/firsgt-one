369e9ab6ab9a11e95e5c1001e9c2e1a4b84b9553
omaiwa-no-shindaigru <116911790+omaiwa-no-shindaigru@users.noreply.github.com>
2022-10-30 00:53:02 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../css/App.module.css";
+import "../css/form.module.css";
+import "../css/weather.module.css";
+import "../css/index.css";
+const App = ({ Component, props }) => {
+  return <Component {...props} />;
+};
+export default App;
